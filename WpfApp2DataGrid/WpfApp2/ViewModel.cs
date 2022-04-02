12a5f5e2be3582ea1fc741309b86e42ae39610ff
kwons <code.sona@gmail.com>
2022-04-03 01:14:58 +0900

    datagrid cell merge

---
+++ WpfApp2DataGrid/WpfApp2/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            this.Countries = new List<Country>();
+            this.Countries.Add(new Country { Name = "Germany", Continent = "Europe" });
+            this.Countries.Add(new Country { Name = "United Kingdom", Continent = "Europe" });
+            this.Countries.Add(new Country { Name = "France", Continent = "Europe" });
+            this.Countries.Add(new Country { Name = "USA", Continent = "North America" });
+            this.Countries.Add(new Country { Name = "Canada", Continent = "North America" });
+        }
+
+        public IList<Country> Countries { get; private set; }
+    }
+
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Continent { get; set; }
+    }
+}
