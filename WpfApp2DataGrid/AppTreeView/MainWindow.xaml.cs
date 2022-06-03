8aaa6a842a91512c8cbcf31179f16a3745c7b1b1
kwons <102870461+kwons05@users.noreply.github.com>
2022-06-03 18:56:20 +0900

    TreeView

---
+++ WpfApp2DataGrid/AppTreeView/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using AppTreeView.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace AppTreeView
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Group> Groups { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+
+            Groups = new ObservableCollection<Group>();
+
+            Group grp1 = new Group() { Key = 1, Name = "Group 1", SubGroups = new List<Group>(), Entries = new List<Entry>() };
+            Group grp2 = new Group() { Key = 2, Name = "Group 2", SubGroups = new List<Group>(), Entries = new List<Entry>() };
+            Group grp3 = new Group() { Key = 3, Name = "Group 3", SubGroups = new List<Group>(), Entries = new List<Entry>() };
+            Group grp4 = new Group() { Key = 4, Name = "Group 4", SubGroups = new List<Group>(), Entries = new List<Entry>() };
+
+            //grp1
+            grp1.Entries.Add(new Entry() { Key = 1, Name = "Entry number 1" });
+            grp1.Entries.Add(new Entry() { Key = 2, Name = "Entry number 2" });
+            grp1.Entries.Add(new Entry() { Key = 3, Name = "Entry number 3" });
+
+            //grp2
+            grp2.Entries.Add(new Entry() { Key = 4, Name = "Entry number 4" });
+            grp2.Entries.Add(new Entry() { Key = 5, Name = "Entry number 5" });
+            grp2.Entries.Add(new Entry() { Key = 6, Name = "Entry number 6" });
+
+            //grp3
+            grp3.Entries.Add(new Entry() { Key = 7, Name = "Entry number 7" });
+            grp3.Entries.Add(new Entry() { Key = 8, Name = "Entry number 8" });
+            grp3.Entries.Add(new Entry() { Key = 9, Name = "Entry number 9" });
+
+            //grp4
+            grp4.Entries.Add(new Entry() { Key = 10, Name = "Entry number 10" });
+            grp4.Entries.Add(new Entry() { Key = 11, Name = "Entry number 11" });
+            grp4.Entries.Add(new Entry() { Key = 12, Name = "Entry number 12" });
+
+            grp4.SubGroups.Add(grp1);
+            grp2.SubGroups.Add(grp4);
+
+            Groups.Add(grp1);
+            Groups.Add(grp2);
+            Groups.Add(grp3);
+        }
+    }
+}
---
+++ WpfApp2DataGrid/AppTreeView/Models/Group.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTreeView.Models
+{
+	// https://stackoverflow.com/questions/1912481/wpf-treeview-hierarchicaldatatemplate-binding-to-object-with-multiple-child-co
+
+	public class Entry
+	{
+		public int Key { get; set; }
+		public string Name { get; set; }
+	}
+
+	public class Group
+	{
+		public int Key { get; set; }
+		public string Name { get; set; }
+
+		public IList<Group> SubGroups { get; set; }
+		public IList<Entry> Entries { get; set; }
+
+		//public IEnumerable<object> Items
+		//{
+		//	get
+		//	{
+		//		foreach (var group in this.SubGroups)
+		//			yield return group;
+		//		foreach (var entry in this.Entries)
+		//			yield return entry;
+		//	}
+		//}
+
+		public IList<object> Items
+		{
+			get
+			{
+				IList<object> childNodes = new List<object>();
+				foreach (var group in this.SubGroups)
+					childNodes.Add(group);
+				foreach (var entry in this.Entries)
+					childNodes.Add(entry);
+
+				return childNodes;
+			}
+		}
+	}
+}
