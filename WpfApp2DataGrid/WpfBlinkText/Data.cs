15fc58c0b0758f274b4e4d7611f775890ae2fe83
kwons <code.sona@gmail.com>
2022-09-19 22:53:59 +0900

    Border Blink

---
+++ WpfApp2DataGrid/WpfBlinkText/Data.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBlinkText
+{
+    public class Data : BindableBase
+    {
+        private int _state;
+        public int State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                SetProperty(ref _state, value);
+            }
+        }
+
+    }
+}
---
+++ WpfApp2DataGrid/WpfBlinkText/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WpfBlinkText
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Data Data { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Data = new Data();
+
+            this.DataContext = this;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Data.State = 1;
+        }
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Data.State = 0;
+        }
+    }
+}
